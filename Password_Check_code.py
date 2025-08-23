52c5a17bf525fe7415588af4d194d63c9b2bc11c
Denzel-Britton <denzel.britton573@gmail.com>
2025-08-23 16:19:56 -0500

    setting up funtions to check again character list

--- Password_Check_code.py
+++ Password_Check_code.py
@@ -1,1 +1,17 @@
 #testing connection to get hub
+
+import string
+#string.ascii_uppercase returns all alphabet in upper case
+#comprehension list [ part1   part2  part3] ----> break it down start part 3 and work backwards   [ transformation(item in are new list ,for var in ,orginal_list]
+
+#  if uppercase letter is found it will add a 1 to c  else c = 0 this  iterates through each char
+psswd = "Helloword"
+upper_case = any([1 if c in string.ascii_uppercase else 0 for c in psswd])
+lower_case = any([1 if c in string.ascii_lowercase else 0 for c in psswd])
+special_char = any([1 if c in string.punctuation else 0 for c in psswd])
+digits = any([1 if c in string.digits else 0 for c in psswd])
+
+print(upper_case)
+characters= [upper_case,lower_case,special_char,digits]
+
+length= len(psswd)
